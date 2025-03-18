af7081b35e950e2642ccb7d633295190429943bf
akshathdeepa1978@gmail.com <akshathdeepa1978@gmail.com>
2025-03-18 14:44:36 +0530

    slarary

---
+++ js_programs/p1.js
@@ -0,0 +1,13 @@
+let inputnum =200
+console.log(`user given number id ${inputnum}`)
+
+root=parseInt(Math.sqrt(inputnum))
+if (inputnum==root)
+{
+    console.log(`user given number is perfect square ${inputnum}`)
+
+}
+else{
+    console.log(`user given number is not perfect square  ${inputnum}`)
+
+}
